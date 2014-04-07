3bd43abf9d204177209857581584e1bbe41bed01
Ken Williams <ken@derbycat.com>
2014-04-07 13:07:40 +0000

    Baby steps.

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,15 @@
 -- | Main entry point to the application.
 module Main where
+import  System.Environment
+import  Control.Monad   (forM_)
 
 -- | The main entry point.
 main :: IO ()
 main = do
     putStrLn "Welcome to FP Haskell Center!"
+    pname <-getProgName
+    args <- getArgs
+    putStrLn pname
+    forM_ args $ \arg ->
+        putStrLn arg
     putStrLn "Have a good day!"
